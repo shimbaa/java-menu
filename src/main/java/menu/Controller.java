b75dcd2e4e6bf8bd6c1e9a88fd97b78053558194
심석현 <shim9597@gmail.com>
2023-09-24 18:39:39 +0900

    feat: 코치 인원 검증

--- src/main/java/menu/Controller.java
+++ src/main/java/menu/Controller.java
@@ -2,6 +2,9 @@ package menu;
 
 public class Controller {
 
+    public static final int MIN_COACH_NUMBER = 2;
+    public static final int MAX_COACH_NUMBER = 5;
+    public static final String INVALID_COACH_NUMBER_MESSAGE = "[ERROR] 코치는 최소 2명, 최대 5명까지 입력 가능 합니다.";
     private final InputView inputView;
 
     public Controller(InputView inputView) {
@@ -10,6 +13,10 @@ public class Controller {
 
     public void setCoaches() {
         inputView.printStartMessage();
-        String coaches = inputView.readCoachNames();
+        String input = inputView.readCoachNames();
+        String[] coachNames = input.trim().split(",");
+        if (coachNames.length < MIN_COACH_NUMBER || coachNames.length > MAX_COACH_NUMBER) {
+            throw new IllegalArgumentException(INVALID_COACH_NUMBER_MESSAGE);
+        }
     }
 }
