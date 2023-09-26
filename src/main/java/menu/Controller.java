d3b0dc40a7eaa3999c1eb1e202fa69d5a691116a
심석현 <shim9597@gmail.com>
2023-09-26 13:55:40 +0900

    refactor: 코치 이름 입력, 변환, 검증 부분 수정

--- src/main/java/menu/Controller.java
+++ src/main/java/menu/Controller.java
@@ -6,15 +6,9 @@ import menu.domain.RecommendStatus;
 import menu.view.InputView;
 import menu.view.OutputView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
-
-    private static final int MIN_COACH_NUMBER = 2;
-    private static final int MAX_COACH_NUMBER = 5;
-    private static final String INVALID_COACH_NUMBER_MESSAGE = "[ERROR] 코치는 최소 2명, 최대 5명까지 입력 가능 합니다.";
-    private static final String COMMA = ",";
 
     public static RecommendStatus recommendStatus;
     private final InputView inputView;
@@ -42,17 +36,7 @@ public class Controller {
     }
 
     private void setCoachName() {
-        String input = inputView.readCoachNames();
-        String[] coachNames = input.split(COMMA);
-
-        if (coachNames.length < MIN_COACH_NUMBER || coachNames.length > MAX_COACH_NUMBER) {
-            throw new IllegalArgumentException(INVALID_COACH_NUMBER_MESSAGE);
-        }
-
-        List<Coach> coaches = new ArrayList<>();
-        for (String coachName : coachNames) {
-            coaches.add(new Coach(coachName.trim()));
-        }
+        List<Coach> coaches = inputView.getCoaches();
         coachRepository = new CoachRepository(coaches);
     }
 
@@ -61,7 +45,7 @@ public class Controller {
         for (Coach coach : coaches) {
             String input = inputView.readDislikeFoods(coach.getName());
             if (!input.isEmpty()) {
-                String[] dislikeFoods = input.trim().split(COMMA);
+                String[] dislikeFoods = input.trim().split(",");
                 coach.addDislikeFood(dislikeFoods);
             }
         }
--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,8 +1,20 @@
 package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import menu.domain.Coach;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
+
+    private static final int MIN_COACH_NUMBER = 2;
+    private static final int MAX_COACH_NUMBER = 5;
+
+    private static final String INVALID_COACH_NUMBER_MESSAGE = "[ERROR] 코치는 최소 2명, 최대 5명까지 입력 가능 합니다.";
+    private static final String COMMA = ",";
+
 
     private enum ConsoleMessage {
         START("점심 메뉴 추천을 시작합니다.\n"),
@@ -20,13 +32,40 @@ public class InputView {
         System.out.println(ConsoleMessage.START.message);
     }
 
-    public String readCoachNames() {
-        System.out.println(ConsoleMessage.COACH_NAME.message);
-        return Console.readLine();
+    public List<Coach> getCoaches() {
+        try {
+            System.out.println(ConsoleMessage.COACH_NAME.message);
+            String input = readInput();
+            String[] names = input.split(COMMA);
+
+            validateCoachNumber(names);
+
+            return toCoaches(names);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return getCoaches();
+        }
     }
 
     public String readDislikeFoods(String coachName) {
         System.out.println(coachName + ConsoleMessage.DISLIKE_FOOD.message);
+        return Console.readLine();
+    }
+
+    private void validateCoachNumber(String[] names) {
+        if (names.length < MIN_COACH_NUMBER || names.length > MAX_COACH_NUMBER) {
+            throw new IllegalArgumentException(INVALID_COACH_NUMBER_MESSAGE);
+        }
+    }
+
+    private List<Coach> toCoaches(String[] names) {
+        return Arrays.stream(names)
+                .map(String::trim)
+                .map(Coach::new)
+                .collect(Collectors.toList());
+    }
+
+    private String readInput() {
         return Console.readLine();
     }
 }
